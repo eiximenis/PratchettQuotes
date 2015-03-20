0f4b9e4b4b9ee5d7ccb5206977a95aea62165405
Eduard Tomàs <etomas@plainconcepts.com>
2015-03-20 11:51:39 +0100

    Initial commit

---
+++ PratchettAppBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratchettQuotes
+{
+    public static class PratchettAppBuilderExtensions
+    {
+        public static void UseTerryPratchett(this IAppBuilder app)
+        {
+            app.Use(typeof(PratchettOwinMiddleware), 
+                new PratchettQuotesFactory(
+                    new InternalFileQuoteParser(),
+                    () =>  Assembly.GetExecutingAssembly().GetManifestResourceStream("PratchettQuotes.terry_quotes.txt")));
+        }
+
+        public static void UseTerryPratchett(this IAppBuilder app, IQuoteParser quoteParser, string filename)
+        {
+            app.Use(typeof(PratchettOwinMiddleware),
+                new PratchettQuotesFactory(quoteParser, () => new FileStream(filename, FileMode.Open, FileAccess.Read)));
+        }
+
+        public static void UseTerryPratchett(this IAppBuilder app, IQuoteParser quoteParser, Func<Stream> quotesProvider)
+        {
+            app.Use(typeof(PratchettOwinMiddleware), new PratchettQuotesFactory(quoteParser, quotesProvider));
+        }
+    }
+}
---
+++ PratchettOwinMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace PratchettQuotes
+{
+    public class PratchettOwinMiddleware
+    {
+        private readonly AppFunc _next;
+        private readonly PratchettQuotesFactory _quotesFactory;
+        private readonly Random _random;
+        public PratchettOwinMiddleware(AppFunc next, PratchettQuotesFactory quotesFactory)
+        {
+            _next = next;
+            _quotesFactory = quotesFactory;
+            _random = new Random();
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            await _next.Invoke(environment);
+            var headers = environment["owin.ResponseHeaders"] as IDictionary<string, string[]>;
+            var quotes = _quotesFactory.GetQuotes();
+            var quote = quotes[_random.Next(0, quotes.Length)];
+            headers.Add("X-Pratchett-Quote", new string[] { quote});
+        }
+    }
+}
